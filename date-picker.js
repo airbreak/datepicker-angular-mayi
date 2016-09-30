f45c54c1215e9504c5cecce142af5685c0aeb27b
jimmy <jiangjianming_m@163.com>
2016-09-30 18:19:13 +0800

    finish selcted derict

--- date-picker.js
+++ date-picker.js
@@ -1,19 +1,71 @@
 
 'use strict';
 
-angular.moudle('date-picker-mayi')
+angular.module('date-picker-mayi',[])
 	.directive('selectMayi',function(){
 		return {
+			scope:{
+				array:'=arraydata',
+				index:'=indexdata'
+			},
 			restrict:'AE',
-			template:'<div><div class="selct-main"><ul><li ng-repeat="item in array"></li></ul></div></div>'
-			replace:true
+			template:'<div class="select-mayi" index="{{index}}">'+
+						'<div class="selct-main-mayi">'+
+							'<span class="prev-mayi"></span>'+
+							'<div class="main-container-mayi">'+
+								'<ul class="ul-container-mayi" >'+
+									'<li ng-repeat="item in array">{{item.name}}</li>'+
+								'</ul>'+
+							'</div>'+
+							'<span class="next-mayi"></span>'+
+						'</div>'+
+					'</div>',
+			replace:true,
+			link:function($scope,$element,$attrs){
+
+				$element[0].querySelector('.prev-mayi').addEventListener('click',function(){
+					var index = $element[0].getAttribute('index') | 0;
+					if(index!==0){
+						index--;
+						$element[0].setAttribute('index',index);
+						swipeToMonth(index);
+					}
+				});
+				$element[0].querySelector('.next-mayi').addEventListener('click',function(){
+					var index = $element[0].getAttribute('index') | 0;
+					if(index!== $scope.array.length-1){
+						index++;
+						$element[0].setAttribute('index',index);
+						swipeToMonth(index);
+					}
+				});
+				function swipeToMonth(index){
+					var diff = index * -73 +'px';
+					$element[0].querySelector('.ul-container-mayi').style.webkitTransform='translate('+diff+')';
+				}
+				function getNowDate(){
+					var now=new Date();
+					return{
+						day:now.getDay(),
+						month:now.getMonth(),
+						years:now.getYear()
+					}
+				}
+				swipeToMonth($scope.index | 0);
+			},
+			controller:function(){
+
+			}
 		}
 	})
 	.directive('datePicker',function(){
 		return {
 			restrict:'A',
-			template:''，
-			replace:true
+			template:'',
+			replace:true,
+			link:function(scope,element,attrs){
+
+			}
 		}
 	})
 
